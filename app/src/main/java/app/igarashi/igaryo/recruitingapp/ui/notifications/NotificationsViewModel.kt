782f90a71f14d06b56d5d6cc21b9c4e678db1aba
Ryo Igarashi <igayro_stationery@outlook.com>
2021-05-23 16:00:35 +0900

    initial commit

---
+++ app/src/main/java/app/igarashi/igaryo/recruitingapp/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package app.igarashi.igaryo.recruitingapp.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
